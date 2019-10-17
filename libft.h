8a2db3aaa6e30eb9285fb0d93e1f882e092ae182
Miikka Tuominen <mtuomine@c2r5p13.hive.fi>
2019-10-17 08:30:22 +0300

    lstdel ready

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 12:46:45 by mtuomine          #+#    #+#             */
-/*   Updated: 2019/10/17 07:56:18 by mtuomine         ###   ########.fr       */
+/*   Updated: 2019/10/17 08:20:21 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,5 +92,6 @@ int					ft_islower(int c);
 
 t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
+void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 
 #endif
