14106ecebf34952b273385584ba780ce5bcfa763
Miikka Tuominen <mtuomine@c2r5p13.hive.fi>
2019-10-17 07:56:50 +0300

    lstdelone fixed

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,14 +6,14 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/16 21:16:52 by mtuomine          #+#    #+#             */
-/*   Updated: 2019/10/16 21:34:16 by mtuomine         ###   ########.fr       */
+/*   Updated: 2019/10/17 07:56:12 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-void	ft_lstdelalone(t_list **alst, void (*del)(void*, size_t))
+void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t))
 {
 	if (*alst)
 	{
