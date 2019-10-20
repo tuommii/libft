2e3b75d71d8e4b719ab8599cda0347e12798c726
Miikka Tuominen <mtuomine@c2r5p13.hive.fi>
2019-10-20 09:32:56 +0300

    fixed error in strrev and everything should work fine

--- ft_strrev.c
+++ ft_strrev.c
@@ -6,17 +6,15 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 07:51:34 by mtuomine          #+#    #+#             */
-/*   Updated: 2019/10/19 08:44:03 by mtuomine         ###   ########.fr       */
+/*   Updated: 2019/10/20 09:31:18 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
 char	*ft_strrev(char *s)
 {
-	int len;
-	int i;
-	char temp;
+	int		len;
+	int		i;
+	char	temp;
 
 	len = 0;
 	while (s[len] != '\0')
