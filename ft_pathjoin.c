877705bc3d866ac08ded222b88bebd3809044111
Miikka Tuominen <tuommii@gmail.com>
2019-12-11 10:12:30 +0200

    fixed segfault with NULLS

--- ft_pathjoin.c
+++ ft_pathjoin.c
@@ -6,7 +6,7 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/11 09:03:59 by mtuomine          #+#    #+#             */
-/*   Updated: 2019/12/11 09:55:36 by mtuomine         ###   ########.fr       */
+/*   Updated: 2019/12/11 10:10:36 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ char	*ft_pathjoin(char *a, char *b)
 	char *temp;
 	char *res;
 
-	if (a && !b[0])
-		return (ft_strdup(a));
-	else if (b && !a[0])
+	if (a == NULL)
+	{
+		if (b == NULL)
+			return (ft_strnew(1));
 		return (ft_strdup(b));
-	else if (!a[0] && !b[0])
-		return (ft_strnew(1));
+	}
+	if (b == NULL)
+		return (ft_strdup(a));
 	temp = NULL;
 	res = NULL;
 	if (!ft_endswith(a, "/"))
